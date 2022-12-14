8b40dc4b5b73a16f7d58ff2703faee9b41a8737f
erax0r <erax0r@gmail.com>
2022-12-14 12:47:08 -0800

    Update ADPShortcutDeployment.ps1

--- ADPShortcutDeployment.ps1
+++ ADPShortcutDeployment.ps1
@@ -1,4 +1,4 @@
-$file = "\\sidomain.local\NETLOGON\ADP\ADP Workforce Now.url"
+$file = "\\{domain}\NETLOGON\ADP\ADP Workforce Now.url"
 $dst = "\\si-fp01\users$\"
 $folder = ""
 foreach ($item in Get-ChildItem($dst) -Directory)
