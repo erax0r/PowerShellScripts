5943ac705156851b4a1dfcc9240d064597a3395a
erax0r <erax0r@gmail.com>
2022-12-15 15:39:53 -0800

    Update ADPShortcutDeployment.ps1

--- ADPShortcutDeployment.ps1
+++ ADPShortcutDeployment.ps1
@@ -1,10 +1,13 @@
-$file = "\\{domain}\NETLOGON\ADP\ADP Workforce Now.url"
-$dst = "\\{server}\users$\"
+$fileName = "ADP Workforce Now.url"
+$filePath = "\\{server}\c$\Temp\"
+$dst = "\\{server}\d$\Users\"
 $folder = ""
 foreach ($item in Get-ChildItem($dst) -Directory)
 {
     $folder = ""
-    $folder = $dst + $item + "\Desktop"
-    Write-Output($folder)
+    $folder = $dst + $item + "\Desktop\"
+    #Copy-Item -Path $file -Destination $folder
+    #Write-Output "Copy-Item -Path $filePath -Destination $folder $fileName"
+    Write-Output "robocopy `"$filePath`" `"$folder`" `"$fileName`" /COPYALL"
+    #robocopy `"$filePath`" `"$folder`" `"$fileName`" /COPYALL
 }
-  
