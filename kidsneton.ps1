3e300b6ca6e883f74dcd3b72c0208e5174034f0f
erax0r <erax0r@gmail.com>
2024-07-17 20:27:51 -0700

    Add files via upload

---
+++ kidsneton.ps1
@@ -0,0 +1,29 @@
+﻿# Define your parameters
+$switchIP = '192.168.1.2'
+$username = 'admin'
+$password = 'cisco' | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
+
+# Set up the SSH session
+$session = New-SSHSession -ComputerName $switchIP -Credential $credential
+$shellstream = New-SSHShellStream -SessionId $session.SessionId
+
+# Define the commands to run
+$commands = @(
+    'enable',
+    'cisco', # Replace with the actual password or a secure method to pass it
+    'conf t',
+    'interface gi1/0/2',
+    'no shut'
+    'exit'
+    'exit'
+    'exit'
+)
+
+# Run the commands
+foreach ($command in $commands) {
+    Invoke-SSHStreamShellCommand -ShellStream $shellstream -Command $command
+}
+
+# Close the SSH session
+Remove-SSHSession -SSHSession $session
